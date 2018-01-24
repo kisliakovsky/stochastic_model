14de53a0aa933ac7229fe76388491bd9fc349be8
kisliakovsky <kisliakovskiii@yandex.ru>
2018-01-24 16:05:49 +0300

    Save

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ from src import exporting
 
 def main():
     model1 = MongooseModel(30, 0.427, 0.001, 6300, 1979, 2000)
-    model2 = MongooseModel(6141, 0.427, 0.6, 6300, 2000, 2011)
-    model3 = AltMongooseModel(6141, 0.01, 552.9, 6300, 2000, 2011)
+    model2 = MongooseModel(6141, 0.427, 0.62, 6300, 2000, 2011)
+    model3 = AltMongooseModel(6141, 0.01, 554.6, 6300, 2000, 2011)
     exporting.save_complex_model_plot(model1, model2, model3)
 
 
